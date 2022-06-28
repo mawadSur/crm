199774782a009d52dc3aaa84b76a21bdaef1b847
Mohammed Awad <mawad@surconsultinggroup.com>
2022-06-28 16:44:13 +0000

    Random update to react_app.js

--- react_app.js
+++ react_app.js
@@ -1,9 +1,6 @@
-// Node.js Server - 2025-02-14T15:39:45.847Z
-import http from 'http';
+// React Component - 2025-02-14T15:39:46.523Z
+import React from 'react';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node.js Server');
-});
-
-server.listen(3000, () => console.log('Server running on port 3000'));
+export default function App() {
+  return <h1>Hello, World!</h1>;
+}
