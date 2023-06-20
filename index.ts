bdd96d73ba271c6bc4ab1a341ec3d25cdf7e3de5
Mohammed\Mohammed Awad <mohammed.awad@underarmour.com>
2023-06-20 08:15:14 -0400

    Added logo and name

--- index.ts
+++ index.ts
@@ -33,8 +33,15 @@ const start = async (): Promise<void> => {
       {
         resource: CarModel
       }
-    ]
-  })
+    ],
+    branding: {
+      companyName: 'Pegasus',
+      favicon: '../public/icon.jpg',
+    },
+    version: {
+      admin: true,
+    },
+  });
 
   const adminRouter = AdminJSExpress.buildRouter(admin)
 
