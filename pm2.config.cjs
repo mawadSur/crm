06bb2946d42a98979beec2e43d3da9609c03dcdb
AstaDK <asta.dev.520@gmail.com>
2023-08-21 21:51:56 +0700

    fix: server build fail

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -9,17 +9,13 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '800M',
+      max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
       },
       env_production: {
         NODE_ENV: 'production',
-        instances: 'max',
-        DATABASE_URL: 'your-production-database-url',
-        SECRET_KEY: 'your-production-secret-key',
       },
-      node_args: ['--max_old_space_size=200'],
     },
   ],
 };
