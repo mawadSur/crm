b152a40746242b5f4d7f5ab23a28ccde1a026123
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-08-26 23:01:39 +0530

    fix: fix the styling on the calculator

--- src/components/calculator/index.tsx
+++ src/components/calculator/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import calculatorStyle from './style.js';
+import domtoimage from 'dom-to-image';
 
 const LoanPaymentMatrix = () => {
   const [marketValue, setMarketValue] = useState('0');
@@ -8,6 +9,7 @@ const LoanPaymentMatrix = () => {
   const [gatavt, setGatavt] = useState('0');
   const [nontaxFees, setNontaxFees] = useState('0');
   const [apr, setApr] = useState('0');
+  const container = useRef(null);
 
   const calculatePayment = (loanTerm) => {
     const principal =
@@ -21,81 +23,221 @@ const LoanPaymentMatrix = () => {
     return payment.toFixed(2);
   };
 
+  const handlePDFDownload = async () => {
+    const img = await domtoimage.toJpeg(container.current, {
+      scale: 4,
+      innerWidth: '100%',
+      innerHeight: '100vh',
+    });
+    var a = document.createElement('a');
+    a.href = img;
+    const imgName = 'download';
+    a.download = `${imgName}.png`;
+    a.click();
+  };
+
   return (
-    <calculatorStyle.CalculatorWrapper>
-      <calculatorStyle.Table>
-        <thead>
-          <tr>
-            <calculatorStyle.TableHeader>Term</calculatorStyle.TableHeader>
-            <calculatorStyle.TableHeader>Monthly Payment</calculatorStyle.TableHeader>
-          </tr>
-        </thead>
-        <tbody>
-          {[48, 60, 72].map((term) => (
-            <calculatorStyle.TableRow key={term}>
-              <calculatorStyle.TableCell>{term} months</calculatorStyle.TableCell>
-              <calculatorStyle.TableCell>${calculatePayment(term)}</calculatorStyle.TableCell>
-            </calculatorStyle.TableRow>
-          ))}
-        </tbody>
-      </calculatorStyle.Table>
+    <>
+      <button
+        style={{
+          width: 'max-content',
+          display: 'flex',
+          alignSelf: 'flex-end',
+        }}
+        onClick={handlePDFDownload}
+      >
+        Download PDF
+      </button>
+      <div
+        id="pdf-container"
+        ref={container}
+        style={{ width: 'calc(100% - 20px)', color: 'black', backgroundColor: 'white' }}
+      >
+        <calculatorStyle.Header>
+          <calculatorStyle.HeaderRow>
+            <calculatorStyle.HeaderCell1>
+              <calculatorStyle.CompanyTitle>PORSCHE</calculatorStyle.CompanyTitle>
+              <calculatorStyle.CompanyName>
+                Hennessy <span style={{ fontWeight: 900 }}>Porsche</span>
+              </calculatorStyle.CompanyName>
+              <calculatorStyle.CompanyDescription>
+                2020 Lorem Ipsum is simply dummy text
+              </calculatorStyle.CompanyDescription>
+            </calculatorStyle.HeaderCell1>
+          </calculatorStyle.HeaderRow>
+          <calculatorStyle.HeaderRow>
+            <calculatorStyle.HeaderCell2>
+              <h4>Date</h4>
+              <calculatorStyle.InputCommon value="1/02/2023" />
+            </calculatorStyle.HeaderCell2>
+            <calculatorStyle.HeaderCell2>
+              <h4>Sales Person</h4>
+              <calculatorStyle.InputCommon value="Rakesh Sharma" />
+            </calculatorStyle.HeaderCell2>
+            <calculatorStyle.HeaderCell2>
+              <h4>Manager</h4>
+              <calculatorStyle.InputCommon value="Rakesh Sharma" />
+            </calculatorStyle.HeaderCell2>
+          </calculatorStyle.HeaderRow>
+        </calculatorStyle.Header>
 
-      <calculatorStyle.InputContainer>
-        <calculatorStyle.Label>
-          Market Value Selling Price:{' '}
-          <calculatorStyle.Input
-            type="number"
-            value={marketValue}
-            onChange={(e) => setMarketValue(e.target.value)}
-          />
-        </calculatorStyle.Label>
-        <calculatorStyle.Label>
-          Taxable Fees:{' '}
-          <calculatorStyle.Input
-            type="number"
-            value={taxableFees}
-            onChange={(e) => setTaxableFees(e.target.value)}
-          />
-        </calculatorStyle.Label>
-        <calculatorStyle.Label>
-          Doc Fee:{' '}
-          <calculatorStyle.Input
-            type="number"
-            value={docFee}
-            onChange={(e) => setDocFee(e.target.value)}
-          />
-        </calculatorStyle.Label>
-        <calculatorStyle.Label>
-          GATAVT:{' '}
-          <calculatorStyle.Input
-            type="number"
-            value={gatavt}
-            onChange={(e) => setGatavt(e.target.value)}
-          />
-        </calculatorStyle.Label>
-        <calculatorStyle.Label>
-          Non-Tax Fees:{' '}
-          <calculatorStyle.Input
-            type="number"
-            value={nontaxFees}
-            onChange={(e) => setNontaxFees(e.target.value)}
-          />
-        </calculatorStyle.Label>
-        <calculatorStyle.Label>
-          APR:{' '}
-          <calculatorStyle.Input
-            type="number"
-            value={apr}
-            onChange={(e) => setApr(e.target.value)}
-          />
-        </calculatorStyle.Label>
-      </calculatorStyle.InputContainer>
+        {/* HeaderBottom */}
 
-      <calculatorStyle.Result>
-        Monthly Payment: ${calculatePayment(60)}{' '}
-        {/* Display the calculated payment for a specific term */}
-      </calculatorStyle.Result>
-    </calculatorStyle.CalculatorWrapper>
+        <calculatorStyle.BottomHeader>
+          <calculatorStyle.BottomFirstDiv>FOR INTERNAL USE ONLY</calculatorStyle.BottomFirstDiv>
+          <calculatorStyle.BottomSecondDiv>
+            <calculatorStyle.BottomThirdDiv>
+              <calculatorStyle.BS1>
+                <calculatorStyle.BSRow>
+                  <h1>CUSTOMER</h1>
+                  <calculatorStyle.InputCommon value="Muhammad Awad" />
+                </calculatorStyle.BSRow>
+                <calculatorStyle.BSRow>
+                  <p>Home Phone</p>
+                  <calculatorStyle.InputCommon value="0987654321" />
+                </calculatorStyle.BSRow>
+                <calculatorStyle.BSRow>
+                  <p>Address</p>
+                  <calculatorStyle.InputCommon value="street-4, New Work" />
+                </calculatorStyle.BSRow>
+              </calculatorStyle.BS1>
+              <calculatorStyle.BS2>
+                <calculatorStyle.BSRow>
+                  <p>Work Phone</p>
+                  <calculatorStyle.InputCommon value="1234567890" />
+                </calculatorStyle.BSRow>
+                <calculatorStyle.BSRow>
+                  <p>E-Mail:</p>
+                  <calculatorStyle.InputCommon value="a@gmail.com" />
+                </calculatorStyle.BSRow>
+                <calculatorStyle.BSRow>
+                  <p>Cell Phone:</p>
+                  <calculatorStyle.InputCommon value="(404)791-9456" />
+                </calculatorStyle.BSRow>
+              </calculatorStyle.BS2>
+            </calculatorStyle.BottomThirdDiv>
+          </calculatorStyle.BottomSecondDiv>
+        </calculatorStyle.BottomHeader>
+
+        {/* MiddleRow */}
+
+        <calculatorStyle.SecondBottom>
+          <calculatorStyle.SB11>
+            <p>VEHICLE</p>
+          </calculatorStyle.SB11>
+          <calculatorStyle.SB22>
+            <calculatorStyle.SB22Child>
+              Stock #:<calculatorStyle.SpanCommon>N847488H</calculatorStyle.SpanCommon>
+            </calculatorStyle.SB22Child>
+            <calculatorStyle.SB22Child>
+              New/Used:<calculatorStyle.SpanCommon>Used</calculatorStyle.SpanCommon>
+            </calculatorStyle.SB22Child>
+            <calculatorStyle.SB22Child>
+              VIN:<calculatorStyle.SpanCommon>7BHDOH8KUGBDJKD</calculatorStyle.SpanCommon>
+            </calculatorStyle.SB22Child>
+            <calculatorStyle.SB22Child>
+              Mileage:<calculatorStyle.SpanCommon>54</calculatorStyle.SpanCommon>
+            </calculatorStyle.SB22Child>
+          </calculatorStyle.SB22>
+          <calculatorStyle.SB33>
+            <calculatorStyle.SB33Child>
+              Vehicle:<calculatorStyle.SpanCommon>2023 Rivian R1S</calculatorStyle.SpanCommon>
+            </calculatorStyle.SB33Child>
+            <calculatorStyle.SB33Child>
+              Color:<calculatorStyle.SpanCommon>Launch Green</calculatorStyle.SpanCommon>
+            </calculatorStyle.SB33Child>
+          </calculatorStyle.SB33>
+          <calculatorStyle.SB44>
+            <p>
+              Type:
+              <calculatorStyle.SpanCommon>
+                Launch Edition All-Wheel Drive Spor
+              </calculatorStyle.SpanCommon>
+            </p>
+          </calculatorStyle.SB44>
+        </calculatorStyle.SecondBottom>
+
+        {/* Calculator */}
+        <calculatorStyle.CalculatorWrapper>
+          <calculatorStyle.Table>
+            <thead>
+              <tr style={{ borderBottom: '1px solid' }}>
+                <td>Load Payments</td>
+                <td>Estimated</td>
+              </tr>
+              <tr style={{ background: '#ccc' }}>
+                <calculatorStyle.TableHeader>Term</calculatorStyle.TableHeader>
+                <calculatorStyle.TableHeader>Monthly Payment</calculatorStyle.TableHeader>
+              </tr>
+            </thead>
+            <tbody>
+              {[48, 60, 72].map((term) => (
+                <calculatorStyle.TableRow key={term}>
+                  <calculatorStyle.TableCell>{term} months</calculatorStyle.TableCell>
+                  <calculatorStyle.TableCell>${calculatePayment(term)}</calculatorStyle.TableCell>
+                </calculatorStyle.TableRow>
+              ))}
+            </tbody>
+          </calculatorStyle.Table>
+
+          <calculatorStyle.InputContainer>
+            <calculatorStyle.InputRow>
+              <calculatorStyle.Label>Market Value Selling Price:</calculatorStyle.Label>
+              <calculatorStyle.Input
+                type="number"
+                value={marketValue}
+                onChange={(e) => setMarketValue(e.target.value)}
+              />
+            </calculatorStyle.InputRow>
+            <calculatorStyle.InputRow>
+              <calculatorStyle.Label>Taxable Fees:</calculatorStyle.Label>
+              <calculatorStyle.Input
+                type="number"
+                value={taxableFees}
+                onChange={(e) => setTaxableFees(e.target.value)}
+              />
+            </calculatorStyle.InputRow>
+            <calculatorStyle.InputRow>
+              <calculatorStyle.Label>Doc Fee:</calculatorStyle.Label>
+              <calculatorStyle.Input
+                type="number"
+                value={docFee}
+                onChange={(e) => setDocFee(e.target.value)}
+              />
+            </calculatorStyle.InputRow>
+            <calculatorStyle.InputRow>
+              <calculatorStyle.Label>GATAVT:</calculatorStyle.Label>
+              <calculatorStyle.Input
+                type="number"
+                value={gatavt}
+                onChange={(e) => setGatavt(e.target.value)}
+              />
+            </calculatorStyle.InputRow>
+            <calculatorStyle.InputRow>
+              <calculatorStyle.Label>Non-Tax Fees:</calculatorStyle.Label>
+              <calculatorStyle.Input
+                type="number"
+                value={nontaxFees}
+                onChange={(e) => setNontaxFees(e.target.value)}
+              />
+            </calculatorStyle.InputRow>
+            <calculatorStyle.InputRow>
+              <calculatorStyle.Label>APR: </calculatorStyle.Label>
+              <calculatorStyle.Input
+                type="number"
+                value={apr}
+                onChange={(e) => setApr(e.target.value)}
+              />
+            </calculatorStyle.InputRow>
+          </calculatorStyle.InputContainer>
+
+          <calculatorStyle.Result>
+            Monthly Payment: ${calculatePayment(60)}{' '}
+            {/* Display the calculated payment for a specific term */}
+          </calculatorStyle.Result>
+        </calculatorStyle.CalculatorWrapper>
+      </div>
+    </>
   );
 };
 
--- src/components/calculator/style.ts
+++ src/components/calculator/style.ts
@@ -1,15 +1,196 @@
 import { styled } from '@adminjs/design-system/styled-components';
 
+// HeaderTop
+const Header = styled.div`
+  display: flex;
+  // border: 1px solid;
+  width: 100%;
+`;
+
+const HeaderRow = styled.div`
+  width: 50%;
+  // border: 1px solid saddlebrown;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+`;
+
+const CompanyTitle = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+`;
+
+const CompanyName = styled.p`
+  font-weight: 100;
+  font-size: 32px;
+  margin-block: 5px;
+`;
+
+const CompanyDescription = styled.p`
+  font-size: 16px;
+`;
+
+const HeaderCell1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`;
+const HeaderCell2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding-bottom: 5px;
+`;
+
+const HeaderCell2Input = styled.input`
+  font-weight: 600;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  outline: none;
+  background-color: inherit;
+`;
+// HeaderBottom
+
+const BottomHeader = styled.div`
+  // border: 1px solid red;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const BottomFirstDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const BottomSecondDiv = styled.div`
+  width: 100%;
+  // display: flex;
+  // flex-direction: row;
+  border-top: 4px solid black;
+  border-bottom: 1px solid black;
+  padding-block: 1px;
+`;
+const BottomThirdDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  border-top: 1px solid black;
+  border-bottom: 4px solid black;
+`;
+
+const BS1 = styled.div`
+  width: 50%;
+  // border: 1px solid;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border-top: 1px solid;
+  padding: 2px;
+`;
+
+const BS2 = styled.div`
+  width: 50%;
+  // border: 1px solid;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border-top: 1px solid;
+  padding: 2px;
+`;
+
+const BSRow = styled.div`
+  // border: 1px solid;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid;
+`;
+
+// MiddleRow
+
+const SecondBottom = styled.div`
+  width: 100%;
+  border-bottom: 4px solid;
+  // border: 1px solid;
+`;
+
+const SB11 = styled.div`
+  width: 100%;
+  // border: 1px solid red;
+  padding: 10px;
+`;
+
+const SB22 = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding-inline: 10px;
+  border-bottom: 1px solid;
+`;
+const SB22Child = styled.div`
+  width: 25%;
+  // border: 1px solid;
+  // padding: 10px;
+`;
+
+const SB33 = styled.div`
+  width: 100%;
+  // border: 1px solid red;
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid;
+`;
+const SB33Child = styled.div`
+  width: 50%;
+  // border: 1px solid;
+  // padding: 10px;
+`;
+
+const SB44 = styled.div`
+  // padding: 10px 0px 0px 0px;
+  width: 100%;
+  border-bottom: 1px solid;
+  margin-bottom: 5px;
+`;
+const SpanCommon = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const InputCommon = styled.input`
+  // flex: 0 0 20%;
+  // padding: 10px;
+  // width:  20%
+  // border-radius: 8px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  outline: none;
+  background-color: inherit;
+  // direction: rtl;
+`;
+
+// calculator
+
 const CalculatorWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 20px;
+  width: 100%;
 `;
 
 const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
-  border: 2px solid #e0e0e0;
+  border: 2px solid;
+  padding: 5px;
 `;
 
 const TableHeader = styled.th`
@@ -31,9 +212,18 @@ const TableCell = styled.td`
 `;
 
 const InputContainer = styled.div`
+  // display: flex;
+  // flex-wrap: wrap;
+  // gap: 20px;
+  // justify-content: space-between;
+  // align-items: center;
+  // width:50%;
+`;
+
+const InputRow = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Label = styled.label`
@@ -48,8 +238,12 @@ const Input = styled.input`
   flex: 0 0 20%;
   padding: 10px;
   width:  20%
-  border: 2px solid #ccc;
   border-radius: 8px;
+  border: none; 
+  border-bottom: 2px solid #ccc; 
+  outline: none; 
+  background-color: inherit;
+  direction: rtl;
 `;
 
 const Result = styled.div`
@@ -63,6 +257,29 @@ const Result = styled.div`
 
 const calculatorStyle = {
   CalculatorWrapper,
+  Header,
+  HeaderRow,
+  HeaderCell1,
+  HeaderCell2,
+  BottomHeader,
+  BottomFirstDiv,
+  BottomSecondDiv,
+  BS1,
+  BS2,
+  BSRow,
+  InputCommon,
+  SpanCommon,
+  CompanyName,
+  CompanyTitle,
+  CompanyDescription,
+  HeaderCell2Input,
+  SecondBottom,
+  SB11,
+  SB22,
+  SB22Child,
+  SB33,
+  SB33Child,
+  SB44,
   Table,
   TableHeader,
   TableRow,
@@ -70,7 +287,9 @@ const calculatorStyle = {
   InputContainer,
   Label,
   Input,
+  InputRow,
   Result,
+  BottomThirdDiv,
 };
 
 export default calculatorStyle;
