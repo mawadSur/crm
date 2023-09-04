8d1e118d41873d7e7eaa473db7a56e915c722c79
rachit-anil <rachit9910102312@gmail.com>
2023-09-04 22:37:57 +0530

    Update environment.ts
    Looking for a fix for this. Making this change so that local does not break for anyone.

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,3 +1,3 @@
 export const ENV_VARIABLES = {
-  APP_URL: process.env.PROD_APP_URL || process.env.APP_URL,
+  APP_URL: 'http://54.242.249.213' || process.env.PROD_APP_URL || process.env.APP_URL, // making this change untill we find a fix
 };
